5cbba7033d5b9fac9ededc57497da58427aa6366
yanggang <yg_2012com@126.com>
2018-05-30 17:43:43 +0800

    增加排序
    Signed-off-by: yanggang <yg_2012com@126.com>

--- spider_gh/test_01.py
+++ spider_gh/test_01.py
@@ -4,15 +4,18 @@ import collections
 # print (os.getcwd())
 f1_path =  'C:\\Users\\Administrator\\PycharmProjects\\spider_gh\\Stock.txt'
 f2_path = 'C:\\Users\\Administrator\\PycharmProjects\\spider_gh\\final.txt'
+
 name_list = []
+#将数据读取出来进行统计并排序
 f1 = open(f1_path, 'ab+' )
 lines = f1.readlines()
 for i in lines:
     name_list.append( i )
 f1.close()
 d = collections.Counter( name_list )
-for k in d:
-   str001 = '%d apper %s'%(d[k],k)
-   f2 = open(f2_path,'ab+')
-   f2.write(str001)
+s=sorted(d.items(),key = lambda x:x[1],reverse = True)
+for a in s:
+    str002 = '%d %s'%(a[1],a[0])
+    f2 = open(f2_path,'ab+')
+    f2.write(str002)
 f2.close()
